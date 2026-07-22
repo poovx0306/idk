a874b6f764802763c06b289078b6e9d93789f46a
ale.py <1725110551@utectulancingo.edu.mx>
2026-07-22 14:22:50 +0000

    Cracion del inicio

---
+++ app.py
@@ -0,0 +1,14 @@
+import web
+# Importamos el controller Inicio desde la carpeta controllers
+from portal_inicio.controllers.inicio import Inicio
+
+# Mapeo de URLs -> Clases
+urls = (
+    '/', 'Inicio',
+    '/inicio', 'Inicio'
+)
+
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()

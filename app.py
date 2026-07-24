6c9202420f9589825bf3d682b4e07105e9ae6f8d
MiguelCoro19 <1725110681@utectulancingo.edu.mx>
2026-07-24 19:23:57 +0000

    cambios agregando estrategias

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ import web
 
 from portal_inicio.controllers.inicio import Inicio
 from inicio_docente.controllers.inicio_docente import InicioDocente
+from estrategias_didacticas.controllers.estrategias_didacticas import EstrategiasDidacticas
 
 urls = (
     '/', 'Inicio',
     '/inicio', 'Inicio',
-    
+
     '/docente/inicio', 'InicioDocente',
-    
+
     '/login/docente', 'LoginDocente',
-    
+
     '/deteccion-temprana', 'DeteccionTemprana',
     '/estrategias-didacticas', 'EstrategiasDidacticas'
 )
 app = web.application(urls, globals())
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run()

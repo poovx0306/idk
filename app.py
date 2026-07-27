be55eed6bb8b8c5181188dea1365c644c07236ea
ale.py <1725110551@utectulancingo.edu.mx>
2026-07-27 17:23:24 +0000

    modulo de registro previo

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from portal_inicio.controllers.inicio import Inicio
 from inicio_docente.controllers.inicio_docente import InicioDocente
 from estrategias_didacticas.controllers.estrategias_didacticas import EstrategiasDidacticas
 from mi_perfil_docente.controllers.mi_perfil import MiPerfil
+from deteccion_temprana.controllers.deteccion import DeteccionTemprana
 
 urls = (
     '/', 'Inicio',

e0c64039d50a98575a448c8102e9b58c1b7bc9ec
ale.py <1725110551@utectulancingo.edu.mx>
2026-08-03 16:59:39 +0000

    Conectar panel de administrativos con base de datos real

--- administrativos/controllers/inicio.py
+++ administrativos/controllers/inicio.py
@@ -4,4 +4,24 @@ render = web.template.render('administrativos/views/')
 
 class InicioAdministrativo:
     def GET(self):
-        return render.inicio()
+        db = web.database(dbn='sqlite', db='sql/conaap.db')
+        
+        total_alumnos = db.query('SELECT COUNT(*) as total FROM infantes')[0].total
+        total_docentes = db.query('SELECT COUNT(*) as total FROM docente')[0].total
+        
+        try:
+            total_cuestionarios = db.query('SELECT COUNT(*) as total FROM cuestionarios')[0].total
+        except:
+            total_cuestionarios = 0
+            
+        try:
+            total_estrategias = db.query('SELECT COUNT(*) as total FROM estrategias_didacticas')[0].total
+        except:
+            total_estrategias = 0
+
+        return render.inicio(
+            alumnos=total_alumnos,
+            docentes=total_docentes,
+            cuestionarios=total_cuestionarios,
+            estrategias=total_estrategias
+        )

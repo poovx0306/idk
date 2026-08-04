65de77e26c0c20458cbb45db7800212f13da94ad
ale.py <1725110551@utectulancingo.edu.mx>
2026-08-04 00:13:21 +0000

    ya sirve administrativos

--- administrativos/controllers/inicio.py
+++ administrativos/controllers/inicio.py
@@ -6,20 +6,37 @@ class InicioAdministrativo:
     def GET(self):
         db = web.database(dbn='sqlite', db='sql/conaap.db')
         
-        total_alumnos = db.query('SELECT COUNT(*) as total FROM infantes')[0].total
-        total_docentes = db.query('SELECT COUNT(*) as total FROM docente')[0].total
-        
+        # Total de infantes (alumnos registrados)
         try:
-            total_cuestionarios = db.query('SELECT COUNT(*) as total FROM cuestionarios')[0].total
+            total_alumnos = db.query('SELECT COUNT(*) as total FROM infantes')[0].total
         except:
-            total_cuestionarios = 0
+            total_alumnos = 0
+        
+        # Total de docentes (verificando si la tabla es 'docente' o 'docentes')
+        try:
+            total_docentes = db.query('SELECT COUNT(*) as total FROM docente')[0].total
+        except:
+            try:
+                total_docentes = db.query('SELECT COUNT(*) as total FROM docentes')[0].total
+            except:
+                total_docentes = 0
+        
+        # Total de cuestionarios respondidos
+        try:
+            total_cuestionarios = db.query('SELECT COUNT(*) as total FROM cuestionario')[0].total
+        except:
+            try:
+                total_cuestionarios = db.query('SELECT COUNT(*) as total FROM cuestionarios')[0].total
+            except:
+                total_cuestionarios = 0
             
+        # Total de estrategias didácticas
         try:
             total_estrategias = db.query('SELECT COUNT(*) as total FROM estrategias_didacticas')[0].total
         except:
             total_estrategias = 0
 
-        # Convertimos la consulta a una lista estándar de Python con list()
+        # Casos pendientes reales
         try:
             casos_pendientes = list(db.query('SELECT * FROM resultados LIMIT 5'))
         except:
